eff352346accc54b1ba26aa8de06547502c599dd
Paul Flint <flint@flint.com>
2011-09-26 08:29:25 -0400

    added code and comments

---
+++ hlcb.sh
@@ -0,0 +1,11 @@
+#!/usr/bin/env xdg-open
+[Desktop Entry]
+Comment=Book Reader 
+Exec=/opt/scr/sys/hlcb.sh
+Icon=/opt/scr/ReaderIcon.ico
+InitialPreference=2
+MimeType=application/x-boo
+Name=Book Reader
+ServiceTypes=
+Terminal=false
+Type=Application
---
+++ hlcs.sh
@@ -0,0 +1,11 @@
+#!/usr/bin/env xdg-open
+[Desktop Entry]
+Comment=Shelf Organizer 
+Exec=/opt/scr/sys/hlcs.sh
+Icon=/opt/scr/sys/closedbks.gif
+InitialPreference=2
+MimeType=application/x-xks;application/x-bks
+Name=Shelf Organizer 
+ServiceTypes=
+Terminal=false
+Type=Application
